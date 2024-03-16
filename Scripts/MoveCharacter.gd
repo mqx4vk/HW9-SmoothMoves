6f5491931da4bb295c2808c7399f19474e69e4c5
Jason Bennett <jasoben@gmail.com>
2024-03-16 09:57:19 -0400

    making hw assignments

--- Scripts/MoveCharacter.gd
+++ Scripts/MoveCharacter.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 var gravity : Vector2
-@export var jump_coefficient : float
-@export var x_speed : float
-@export var x_air_coefficient : float # Should the player move more slowly left and right in the air? Set to zero for no movement, 1 for the same
-@export var x_speed_limit : float # What is the max speed? 
-@export var x_friction : float 
+@export var jump_height : float ## How high should they jump?
+@export var movement_speed : float ## How fast can they move?
+@export var horizontal_air_coefficient : float ## Should the player move more slowly left and right in the air? Set to zero for no movement, 1 for the same
+@export var speed_limit : float ## What is the player's max speed? 
+@export var friction : float ## What friction should they experience on the ground?
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,26 +15,26 @@ func _ready():
 func _get_input():
 	if is_on_floor():
 		if Input.is_action_pressed("move_left"):
-			velocity += Vector2(-x_speed,0)
+			velocity += Vector2(-movement_speed,0)
 		if Input.is_action_pressed("move_right"):
-			velocity += Vector2(x_speed,0)
+			velocity += Vector2(movement_speed,0)
 		if Input.is_action_just_pressed("jump"): # Jump only happens when we're on the floor (unless we want a double jump, but we won't use that here)
-			velocity += Vector2(1,-jump_coefficient)
+			velocity += Vector2(1,-jump_height)
 	if not is_on_floor():
 		if Input.is_action_pressed("move_left"):
-			velocity += Vector2(-x_speed * x_air_coefficient,0)
+			velocity += Vector2(-movement_speed * horizontal_air_coefficient,0)
 		if Input.is_action_pressed("move_right"):
-			velocity += Vector2(x_speed * x_air_coefficient,0)
+			velocity += Vector2(movement_speed * horizontal_air_coefficient,0)
 
 func _limit_speed():
-	if velocity.x > x_speed_limit:
-		velocity = Vector2(x_speed_limit, velocity.y)
-	if velocity.x < -x_speed_limit:
-		velocity = Vector2(-x_speed_limit, velocity.y)
+	if velocity.x > speed_limit:
+		velocity = Vector2(speed_limit, velocity.y)
+	if velocity.x < -speed_limit:
+		velocity = Vector2(-speed_limit, velocity.y)
 
 func _apply_friction():
 	if is_on_floor() and not (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
-		velocity -= Vector2(velocity.x * x_friction, 0)
+		velocity -= Vector2(velocity.x * friction, 0)
 		if abs(velocity.x) < 5:
 			velocity = Vector2(0, velocity.y) # if the velocity in x gets close enough to zero, we set it to zero
 
